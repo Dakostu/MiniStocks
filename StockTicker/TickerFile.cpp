f0fae77ae5743b1b04ffbf74cf0667305f8e61a7
Dakostu <Dkostuj@gmx.de>
2019-06-29 00:00:31 +0200

    finalized skeleton for TickerFile class

--- StockTicker/TickerFile.cpp
+++ StockTicker/TickerFile.cpp
@@ -5,6 +5,25 @@ TickerFile::TickerFile(constStr &fileName){
 }
 
 
+
+bool TickerFile::checkifFile(constStr &filename) {
+    return false;
+}
+
+std::vector<QString> TickerFile::loadFile(constStr &filename) {
+
+    return {};
+}
+
+bool TickerFile::saveFile(constStr &filename, std::vector<QString> vec) {
+    return false;
+}
+
+void TickerFile::makeSaveDir() {
+
+}
+
+
 constStr TickerFile::getSaveDir() {
     return (QStandardPaths::writableLocation(QStandardPaths::HomeLocation))
             + "/.ministocks";  // location of all MiniStocks save and temp files
