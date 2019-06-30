c50946bac62acb0eec5f2d9cdd9a78e427cd9441
Dakostu <Dkostuj@gmx.de>
2019-06-30 15:18:31 +0200

    added skeleton to TickerInfoDownloader

--- StockTicker/TickerInfoDownloader.cpp
+++ StockTicker/TickerInfoDownloader.cpp
@@ -1,6 +1,19 @@
 #include "TickerInfoDownloader.h"
 
-TickerInfoDownloader::TickerInfoDownloader()
+TickerInfoDownloader::TickerInfoDownloader(const QString &ticker) : tickerSymbol(ticker)
 {
+
+}
+
+
+void TickerInfoDownloader::downloadData(const QString &url, const QString &filepath) {
+
+}
+
+void TickerInfoDownloader::parseCSVintoVector(std::istream& csv) {
+
+}
+
+void TickerInfoDownloader::downloadAndParseCSVFile(const QString &ticker) {
 
 }
--- StockTicker/TickerInfoDownloader.h
+++ StockTicker/TickerInfoDownloader.h
@@ -1,11 +1,28 @@
 #ifndef TICKERINFODOWNLOADER_H
 #define TICKERINFODOWNLOADER_H
 
+#ifdef HAS_CURL
+    #include "curl/curl.h"
+#endif
+
+#include <QString>
+#include <fstream>
 
 class TickerInfoDownloader
 {
-public:
-    TickerInfoDownloader();
+#ifdef HAS_CURL
+    CURL *curl;
+    FILE *fp;
+#endif
+
+    const QString tickerSymbol;
+
+public:    
+    TickerInfoDownloader(const QString &ticker);
+
+    void downloadData(const QString &url, const QString &filepath);
+    void parseCSVintoVector(std::istream& csv);
+    void downloadAndParseCSVFile(const QString &ticker);
 };
 
 #endif // TICKERINFODOWNLOADER_H
